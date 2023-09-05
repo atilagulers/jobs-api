51de199249a8f2eec0134363aacaea4cc14e251a
atilagulers <atilagulerofficial@gmail.com>
2023-09-06 01:51:10 +0300

    -

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {register, login} = require('../controllers/auth');
 
 router.post('/register', register);
+
 router.post('/login', login);
 
 module.exports = router;
