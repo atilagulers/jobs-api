7f0552a5a01d95719a93115bffb0cafcdfc09cde
atilagulers <atilagulerofficial@gmail.com>
2023-09-06 14:15:02 +0300

    -

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
-const helmet = require('helmet');
 
-app.use(helmet());
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // connectDB
 const connectDB = require('./db/connect');
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,48 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const {CustomAPIError} = require('../errors');
+const {StatusCodes} = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  const customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong.',
+  };
 
-module.exports = errorHandlerMiddleware
+  //if (err instanceof CustomAPIError) {
+  //  return res.status(err.statusCode).json({msg: err.message});
+  //}
+
+  if (err.name == 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((field) => field.message)
+      .join(', ');
+
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.fields = Object.keys(err.errors);
+
+    return res
+      .status(customError.statusCode)
+      .json({msg: customError.msg, fields: customError.fields});
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.fields = Object.keys(err.keyValue);
+
+    return res
+      .status(customError.statusCode)
+      .json({msg: customError.msg, fields: customError.fields});
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  return res.status(customError.statusCode).json({msg: customError.msg});
+  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err});
+};
+
+module.exports = errorHandlerMiddleware;
