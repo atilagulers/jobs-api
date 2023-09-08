69351739630a396774dbb173b9ca5446b19475a8
atilagulers <atilagulerofficial@gmail.com>
2023-09-08 03:54:28 +0300

    -

--- app.js
+++ app.js
@@ -30,15 +30,16 @@ const authenticateUser = require('./middleware/authentication');
 
 // routers
 const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/jobs');
+const jobsRouter = require('./routes/ ');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-// extra packages
-
 // routes
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
